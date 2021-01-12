5b8d098c7d202b50bc6711d2a0a42cb54df07fc5
Mihir <74041830+mihirs1797@users.noreply.github.com>
2021-01-12 20:03:56 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,17 +1,111 @@
-const express = require("express");
-const connectDB = require("./config/db");
-const shortid = require("shortid");
-const validUrl = require("valid-url");
-const shortUrlRoute = require("./routes/shorturl")
-const getShortenUrlRoute = require("./routes/getshortenurl")
+const express = require('express');
+const bodyParser = require('body-parser');
+const shortid = require('shortid');
+const dns = require('dns');
+const {
+    MongoClient
+} = require('mongodb');
+const cors = require('cors');
+
+
+if (process.env.NODE_ENV !== "production")
+    require('dotenv').config();
+
+const client = new MongoClient(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const app = express();
-connectDB();
 
-app.use(express.json({}));
-const PORT = 8000;
-app.use("/v1/",getShortenUrlRoute)
-app.use("/v1/shorturl", shortUrlRoute);
-app.listen(PORT, () => console.log("Server is listening on port " + PORT));
+app.use(cors());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', async (req, res)=>{
+
+    try{
+        await client.connect();
+
+        let urls = await client.db('url_shortner').collection('url').find().toArray();
+        //await client.close();
+
+        res.json(urls);
+    }catch(err){
+        res.statusCode = 500;
+        res.json({"error": "something went wrong"});
+    }
+
+});
+
+
+app.post('/', (req, res)=>{
+    try{
+        let inputUrl = new URL(req.body.url);
+        dns.lookup(inputUrl.hostname,{all: true}, async (err, address)=>{
+            if(err){
+                res.json({"error": "URL does not exist on internet"});
+            }else{
+                try{
+                    await client.connect();
+            
+                    let doc = await client.db('url_shortner').collection('url').insertOne({
+                        "url": req.body.url,
+                        "short": shortid.generate(),
+                        "clicks": 0
+                    });
+            
+                    if(doc.insertedCount = 1){
+                        res.statusCode = 201;
+                        res.json({"inserted" : true});
+                    }else{
+                        res.statusCode = 500;
+                        res.json({"inserted" : false});
+                    }
+            
+                    //await client.close();
+                }catch(err){
+                    res.statusCode = 500;
+                    res.json({"error": "something went wrong"});
+                }
+            }
+        })
+        
+    }catch(err){
+        res.statusCode = 409;
+        res.json({"error": "url not in correct format. Please mention \'http://\' or \'https://\'"})
+    }
+});
+
+app.get('/:short_url',async (req, res)=>{
+    try{
+        await client.connect();
+
+        let doc = await client.db('url_shortner').collection('url').findOne({short: req.params.short_url});
+        console.log(doc);
+        let updateOutput = await client.db('url_shortner').collection('url').updateOne({short: req.params.short_url}, {$set: {clicks : doc.clicks+1}});
+
+        if(updateOutput.result.nModified === 1){
+            res.redirect(301, doc.url);
+        }
+        else{
+            res.statusCode = 500;
+            res.json({"error" : "clicks not modified"});
+        }
+        //await client.close();
+
+    }catch(err){
+        res.statusCode = 500;
+        res.json({"error": "something went wrong"});
+    }
+});
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log("server started at Port : ", process.env.PORT);
+});
 
 
